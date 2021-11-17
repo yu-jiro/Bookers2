acf0a199044533cdc272cf347f0f5a516bfee687
yu-jiro <gyurujiro@gmail.com>
2021-11-17 02:34:01 +0000

    編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,10 +41,10 @@ class UsersController < ApplicationController
   end
   
   def set_user
-     @user = User.find(params[:id])
-      if @user.id != current_user.id
-        redirect_to user_path(current_user.id)
-      end
+    @user = User.find(params[:id])
+    unless @user = current_user
+      redirect_to user_path(current_user)
+    end
   end
   
 end
